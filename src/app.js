ee8cefb59189d34f3339b7af172dd38e94391aa5
jattoabdul <jattoade@gmail.com>
2018-10-01 21:26:06 +0100

    fix for es6 polyfill

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import 'babel-polyfill';
 import express from 'express';
 import logger from 'morgan';
 import bodyParser from 'body-parser';
@@ -8,18 +9,6 @@ import routes from './api/routers/routes';
 // Set up the express app
 const app = express();
 // const env = process.env.NODE_ENV || 'development';
-
-// if (env === 'production') {
-//   // for serving static react client app on heroku
-//   // app.use('/', express.static(path.resolve(__dirname, '../../client/dist')));
-//   // app.use('/favicon.ico',
-//   // express.static(path.resolve(__dirname, '../../client/dist')));
-// } else {
-//   // for serving static react client app on server localhost:port
-//   // app.use('/', express.static(path.resolve(__dirname, '../client/dist')));
-//   // app.use('/favicon.ico',
-//   //   express.static(path.resolve(__dirname, '../client/src/favicon.ico')));
-// }
 
 app.use(logger('dev')); // Log requests to the console.
 app.use(bodyParser.json());
