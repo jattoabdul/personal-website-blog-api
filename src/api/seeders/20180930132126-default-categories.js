c2bb021415dd9543c4447a185c231a2450cb6050
jattoabdul <jattoade@gmail.com>
2018-12-06 21:54:26 +0100

    add category

--- src/api/seeders/20180930132126-default-categories.js
+++ src/api/seeders/20180930132126-default-categories.js
@@ -3,7 +3,25 @@ module.exports = {
     return queryInterface.bulkInsert('Categories', [
       {
         name: 'General',
-        desc: 'Default General Posts',
+        desc: 'General Articles',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Law',
+        desc: 'Law Related Articles',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Islamic',
+        desc: 'Islamic Articles',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {
+        name: 'Motivational',
+        desc: 'Motivational Articles',
         createdAt: new Date(),
         updatedAt: new Date()
       }
